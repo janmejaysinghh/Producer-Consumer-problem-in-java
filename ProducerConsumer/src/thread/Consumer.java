f2157bae0befab42e46b24f676323cf85e3c6b74
thejanmejaysingh <janmejaysighbhumi000@gmail.com>
2023-10-13 16:01:24 +0530

    producerconsumer

---
+++ ProducerConsumer/src/thread/Consumer.java
@@ -0,0 +1,24 @@
+package thread;
+
+public class Consumer extends Thread {
+
+	Company c;
+
+	Consumer(Company c) {
+		this.c = c;
+	}
+
+	public void run() {
+		{
+
+			while (true) {
+				this.c.consume_item();
+				try {
+					Thread.sleep(2000);
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			}
+		}
+	}
+}
